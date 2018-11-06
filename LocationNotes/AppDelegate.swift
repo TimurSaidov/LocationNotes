22fbdfad3f8bbc372f61eb7696865973ff77e0a7
TimurSaidov <tim77777@inbox.ru>
2018-11-06 22:41:22 +0300

    Creating a new folders

--- LocationNotes/AppDelegate.swift
+++ LocationNotes/AppDelegate.swift
@@ -20,12 +20,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         print(CoreDataManager.shared.persistentContainer.persistentStoreCoordinator, CoreDataManager.shared.persistentContainer.persistentStoreDescriptions)
         
-        let newFolder = Folder.newFolder(name: "A")
-        let newNoteInNewFolder = newFolder.addNote().name = "New note"
-        CoreDataManager.shared.saveContext()
-        
-        print(folders.count)
-        print(notes.count)
+//        let newFolder = Folder.newFolder(name: "A")
+//        let newNoteInNewFolder = newFolder.addNote().name = "New note"
+//        CoreDataManager.shared.saveContext()
+//
+//        print(folders.count)
+//        print(notes.count)
         
         return true
     }
---
+++ LocationNotes/FoldersTableViewController.swift
@@ -0,0 +1,109 @@
+//
+//  FoldersTableViewController.swift
+//  LocationNotes
+//
+//  Created by Timur Saidov on 06/11/2018.
+//  Copyright © 2018 Timur Saidov. All rights reserved.
+//
+
+import UIKit
+
+class FoldersTableViewController: UITableViewController {
+
+    @IBAction func addButtonAction(_ sender: UIBarButtonItem) {
+        let alertConroller = UIAlertController(title: "Create new folder", message: "", preferredStyle: .alert)
+        alertConroller.addTextField { (textField) in
+            textField.placeholder = "Folder name"
+        }
+        
+        let createAction = UIAlertAction(title: "Create", style: .default) { (action) in
+            guard let folderName = alertConroller.textFields![0].text else { return }
+            guard folderName != "" else { return }
+            
+            let _ = Folder.newFolder(name: folderName)
+            self.tableView.reloadData()
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertConroller.addAction(createAction)
+        alertConroller.addAction(cancel)
+        present(alertConroller, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return folders.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellFolder", for: indexPath)
+
+        let folder = folders[indexPath.row]
+        cell.textLabel?.text = folder.name
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
